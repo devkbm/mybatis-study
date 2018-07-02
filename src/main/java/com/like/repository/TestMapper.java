0054984251af268b2d77cdaea00a7d8dcf5f21bc
kbm <kbm0417@nate.com>
2018-07-02 23:45:50 +0900

    Sample Code

---
+++ src/main/java/com/like/repository/TestMapper.java
@@ -0,0 +1,13 @@
+package com.like.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TestMapper {
+
+	List<Map<String, String>> getUserList(Map<String, String> map);
+	
+}
