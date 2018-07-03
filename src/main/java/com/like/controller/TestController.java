29a6b9d801259b8b7556bfda79cc307520c51d72
kbm <kbm0417@nate.com>
2018-07-03 23:09:49 +0900

    test controller 추가

---
+++ src/main/java/com/like/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.like.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.like.repository.TestMapper;
+
+@RestController
+public class TestController {
+
+	@Resource
+	private TestMapper mapper;
+	
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		
+		mapper.getUserList(null);
+		return new ResponseEntity<String>("test", HttpStatus.OK);
+	}
+}
